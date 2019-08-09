b8ff322c016770ef4d6528730795cf1143f23a0d
zhuodongming <605395451@qq.com>
2019-08-09 16:01:38 +0800

    Add ParkPocoGen project

---
+++ src/Park/ParkPocoGen/Program.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using PocoGen;
+using System;
+using System.IO;
+
+namespace ParkPocoGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Park
+            string strPKCConn = "Database=park;Data Source=127.0.0.1;User Id=root;Password=root;charset=utf8;SslMode=none;allowPublicKeyRetrieval=true;";
+            MySqlConnection pkcConn = new MySqlConnection(strPKCConn);
+            PocoGenClient client = new PocoGenClient(options =>
+             {
+                 options.IsGenNPoco = true;
+                 options.Namespace = "PKC.Repository.PKC";
+             });
+            var pkcResult = client.GenerateAllTables(pkcConn);
+            File.WriteAllText("../../../../Park.Repository/Park.cs", pkcResult);
+        }
+    }
+}
