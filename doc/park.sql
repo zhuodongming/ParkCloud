5874b6ff7e2b0772cd2a0104ee387fdca54c6154
zhuodongming <605395451@qq.com>
2019-08-22 15:17:11 +0800

    add park.sql

---
+++ doc/park.sql
@@ -0,0 +1,125 @@
+﻿
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for application
+-- ----------------------------
+DROP TABLE IF EXISTS `application`;
+CREATE TABLE `application`  (
+  `app_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'app_id',
+  `app_key` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'app_key',
+  `app_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '应用名称',
+  `effective_time` datetime(0) NOT NULL COMMENT '生效时间',
+  `expiry_time` datetime(0) NOT NULL COMMENT '失效时间',
+  `status` bit(1) NOT NULL COMMENT '状态 0:禁用，1:启用',
+  `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '备注',
+  `create_time` datetime(0) NOT NULL,
+  `modify_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`app_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for blacklist
+-- ----------------------------
+DROP TABLE IF EXISTS `blacklist`;
+CREATE TABLE `blacklist`  (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `park_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车场id',
+  `park_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车场名称',
+  `plate_no` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车牌号码',
+  `plate_color` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车牌颜色',
+  `effective_time` datetime(0) NOT NULL COMMENT '生效时间',
+  `expiry_time` datetime(0) NOT NULL COMMENT '失效时间',
+  `creater` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '创建者',
+  `status` bit(1) NOT NULL COMMENT '状态 0:禁用，1:启用',
+  `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '备注',
+  `create_time` datetime(0) NOT NULL,
+  `modify_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for entry
+-- ----------------------------
+DROP TABLE IF EXISTS `entry`;
+CREATE TABLE `entry`  (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `req_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '请求id',
+  `park_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车场id',
+  `park_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车场名称',
+  `plate_no` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车牌号码',
+  `plate_color` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车牌颜色',
+  `entry_time` datetime(0) NOT NULL COMMENT '入场时间',
+  `entry_no` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '入口编号',
+  `vehicle_type` tinyint(4) NOT NULL COMMENT '车辆类型',
+  `pass_mode` tinyint(4) NOT NULL COMMENT '通行方式',
+  `pic_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '图片url',
+  `create_time` datetime(0) NOT NULL,
+  `modify_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for leave
+-- ----------------------------
+DROP TABLE IF EXISTS `leave`;
+CREATE TABLE `leave`  (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `req_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '请求id',
+  `park_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车场id',
+  `park_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车场名称',
+  `plate_no` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车牌号码',
+  `plate_color` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车牌颜色',
+  `entry_time` datetime(0) NOT NULL COMMENT '入场时间',
+  `entry_no` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '入口编号',
+  `leave_time` datetime(0) NULL DEFAULT NULL COMMENT '离场时间',
+  `exit_no` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '出口编号',
+  `parking_time` int(11) NULL DEFAULT NULL COMMENT '停车时长 单位:分钟',
+  `receivable` bigint(20) NULL DEFAULT NULL COMMENT '应收 单位：分',
+  `paid` bigint(20) NULL DEFAULT NULL COMMENT '实收 单位：分',
+  `discount` bigint(20) NULL DEFAULT NULL COMMENT '折扣 单位：分',
+  `vehicle_type` tinyint(4) NOT NULL COMMENT '车辆类型',
+  `pass_mode` tinyint(4) NOT NULL COMMENT '通行方式',
+  `pic_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '图片url',
+  `create_time` datetime(0) NOT NULL,
+  `modify_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for park
+-- ----------------------------
+DROP TABLE IF EXISTS `park`;
+CREATE TABLE `park`  (
+  `park_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车场id',
+  `park_key` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车场key',
+  `park_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车场名称',
+  `status` bit(1) NOT NULL COMMENT '状态 0:禁用，1:启用',
+  `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '备注',
+  `create_time` datetime(0) NOT NULL,
+  `modify_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`park_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for passport
+-- ----------------------------
+DROP TABLE IF EXISTS `passport`;
+CREATE TABLE `passport`  (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `park_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车场id',
+  `park_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车场名称',
+  `plate_no` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车牌号码',
+  `plate_color` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '车牌颜色',
+  `effective_time` datetime(0) NOT NULL COMMENT '生效时间',
+  `expiry_time` datetime(0) NOT NULL COMMENT '失效时间',
+  `creater` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '创建者',
+  `status` bit(1) NOT NULL COMMENT '状态 0:禁用，1:启用',
+  `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '备注',
+  `create_time` datetime(0) NOT NULL,
+  `modify_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
